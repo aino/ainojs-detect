14d46ced2e5e9fa090ca8156ffc2fbcc54894eac
David Hellsing <david@aino.se>
2015-04-15 17:26:33 +0200

    Building minor from version v1.0.7

--- index.js
+++ index.js
@@ -2,77 +2,82 @@
  * Detects various browser features and vendors
  */
 
-var ua = navigator.userAgent.toLowerCase()
-var isWebkit = ua.indexOf( "applewebkit" ) > -1
-var platform = navigator.platform
+if ( typeof window == 'undefined' ) {
+  module.exports = {} // no detection for node
+} else {
 
-module.exports = {
+  var ua = navigator.userAgent.toLowerCase()
+  var isWebkit = ua.indexOf( "applewebkit" ) > -1
+  var platform = navigator.platform
 
-  touch: !!('ontouchstart' in document),
+  module.exports = {
 
-  canvas: !!('getContext' in document.createElement('canvas')),
+    touch: !!('ontouchstart' in document),
 
-  blendMode: document.documentElement && typeof window.getComputedStyle(document.documentElement).backgroundBlendMode != 'undefined',
+    canvas: !!('getContext' in document.createElement('canvas')),
 
-  svg: (function() {
-    return !! document.createElementNS &&
-           !! document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect
-  }()),
+    blendMode: document.documentElement && typeof window.getComputedStyle(document.documentElement).backgroundBlendMode != 'undefined',
 
-  ie: (function() {
+    svg: (function() {
+      return !! document.createElementNS &&
+             !! document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect
+    }()),
 
-    var v = 3
-    var div = document.createElement( 'div' )
-    var all = div.getElementsByTagName( 'i' )
+    ie: (function() {
 
-    do
-      div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->'
-    while 
-      (all[0])
-    return v > 4 ? v : document.documentMode
+      var v = 3
+      var div = document.createElement( 'div' )
+      var all = div.getElementsByTagName( 'i' )
 
-  }()),
+      do
+        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->'
+      while 
+        (all[0])
+      return v > 4 ? v : document.documentMode
 
-  iphone: isWebkit && /iPhone/.test( platform ),
-  ipad:   isWebkit && /iPad/.test( platform ),
-  ipod:   isWebkit && /iPod/.test( platform ),
-  ios:    isWebkit && /iPhone|iPad|iPod/.test( platform ),
-  safari: !!(/safari/.test(ua) && !(/chrome/.test(ua))),
-  chrome: /chrome/.test(ua),
+    }()),
 
-  mac: platform.toUpperCase().indexOf('MAC') >= 0,
+    iphone: isWebkit && /iPhone/.test( platform ),
+    ipad:   isWebkit && /iPad/.test( platform ),
+    ipod:   isWebkit && /iPod/.test( platform ),
+    ios:    isWebkit && /iPhone|iPad|iPod/.test( platform ),
+    safari: !!(/safari/.test(ua) && !(/chrome/.test(ua))),
+    chrome: /chrome/.test(ua),
 
-  retina: (function() {
-    var dp = window.devicePixelRatio
-    var mm = window.matchMedia
-    if ( mm ) {
-      var mq = mm("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)")
-      return !!( (mq && mq.matches) || dp > 1 )
-    } 
-    return dp ? dp > 1 : false
-  }()),
-  
-  translate3d: (function() {
-    var el = document.createElement('p')
-    var has3d
-    var transforms = {
-        'webkitTransform':'-webkit-transform',
-        'OTransform':'-o-transform',
-        'msTransform':'-ms-transform',
-        'MozTransform':'-moz-transform',
-        'transform':'transform'
-    }
+    mac: platform.toUpperCase().indexOf('MAC') >= 0,
 
-    document.documentElement.insertBefore(el, null)
-
-    for (var t in transforms) {
-      if ( typeof el.style[t] != 'undefined' ) {
-        el.style[t] = 'translate3d(1px,1px,1px)'
-        has3d = window.getComputedStyle(el).getPropertyValue(transforms[t])
+    retina: (function() {
+      var dp = window.devicePixelRatio
+      var mm = window.matchMedia
+      if ( mm ) {
+        var mq = mm("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)")
+        return !!( (mq && mq.matches) || dp > 1 )
+      } 
+      return dp ? dp > 1 : false
+    }()),
+    
+    translate3d: (function() {
+      var el = document.createElement('p')
+      var has3d
+      var transforms = {
+          'webkitTransform':'-webkit-transform',
+          'OTransform':'-o-transform',
+          'msTransform':'-ms-transform',
+          'MozTransform':'-moz-transform',
+          'transform':'transform'
       }
-    }
 
-    document.documentElement.removeChild(el)
-    return (has3d !== undefined && has3d.length > 0 && has3d !== "none")
-  }())
+      document.documentElement.insertBefore(el, null)
+
+      for (var t in transforms) {
+        if ( typeof el.style[t] != 'undefined' ) {
+          el.style[t] = 'translate3d(1px,1px,1px)'
+          has3d = window.getComputedStyle(el).getPropertyValue(transforms[t])
+        }
+      }
+
+      document.documentElement.removeChild(el)
+      return (has3d !== undefined && has3d.length > 0 && has3d !== "none")
+    }())
+  }
 }
